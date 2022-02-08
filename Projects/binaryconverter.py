ed421f9370cb45995c254ded31ae366c63789906
Arnav Bhagwat <arnavdoescoding@gmail.com>
2022-02-08 22:21:52 +0530

    Binary Code Converter

---
+++ Projects/binaryconverter.py
@@ -0,0 +1,14 @@
+user_data = input("Enter your binary number, kindly enter '.00' if there are no decimal points: ")
+empty_list = list()
+for digit in user_data :
+    empty_list.append(digit)
+dot_index = empty_list.index('.')
+pre_dot_number = empty_list[: dot_index]
+post_dot_number = empty_list[(dot_index + 1):]
+pre_decimal_total = 0
+while dot_index >= 0:
+    for element in pre_dot_number:
+        current_number = ((2**(dot_index))*int(element))
+        pre_decimal_total = pre_decimal_total + current_number
+print(pre_decimal_total)
+
