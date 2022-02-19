86139cc30a687b41a5d5ef577eb12c648222c70f
Arnav Bhagwat <arnavdoescoding@gmail.com>
2022-02-19 21:41:38 +0530

    A

---
+++ Basic Projects/cardgenerator.py
@@ -0,0 +1,14 @@
+from playingcards import Deck , Card
+import random 
+
+while True:
+    call = input('Enter N to terminate: ')
+    if call == '':
+
+        deck_numeral = random.randint(0,13) 
+        suit_numeral = random.randint(0 , 3)
+        player_card = Card ( value= deck_numeral , suit= suit_numeral )
+        print(player_card.img)
+        continue
+    else:
+        break
