984194ff48ff8f0885c34c73561df7bc61035eb0
Arnav Bhagwat <arnavdoescoding@gmail.com>
2022-02-21 07:39:38 +0530

    Turtle

---
+++ Coursera Michigan/turtle.py
@@ -0,0 +1,12 @@
+import turtle 
+
+wn = turtle.Screen()
+turt = turtle.Turtle()
+turt.forward(150)
+turt.left(90)
+turt.forward(100)
+turt.left(90)
+turt.forward(150)
+turt.left(90)
+turt.forward(100)
+
