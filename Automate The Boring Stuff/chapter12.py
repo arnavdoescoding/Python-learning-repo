d6a46401ca347934f549e29531e5141f7f1bfadf
Arnav Bhagwat <arnavdoescoding@gmail.com>
2022-02-21 09:27:00 +0530

    Selenium

--- Automate The Boring Stuff/chapter12.py
+++ Automate The Boring Stuff/chapter12.py
@@ -1,10 +1,4 @@
-import requests , bs4 
-url = 'https://www.coursera.org/programs/manipal-education-tguaf?currentTab=CATALOG'
-res = requests.get(url)
-example_soup = bs4.BeautifulSoup(res.text, 'lxml')
-elems = example_soup.select('body')
-len_parse = len(elems) 
-n = 0 
-while n < len_parse :
-    print(elems[n].getText())
-    n = n + 1
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+type(browser)
