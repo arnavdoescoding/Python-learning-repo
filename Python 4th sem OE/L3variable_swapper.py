83d32836d4e60a72b908fe5d4fd340eb17dee72a
Arnav Bhagwat <arnavdoescoding@gmail.com>
2022-03-02 17:05:42 +0530

    Lec

---
+++ Python 4th sem OE/L3variable_swapper.py
@@ -0,0 +1,10 @@
+#variable swapping 
+
+x = input('Enter the value of x: ')
+y = input('Enter the value of y: ')
+
+def variable_swapper(a , b):
+    print('New value of x is:', b)
+    print('New value of y is:', a)
+
+variable_swapper(x , y)
