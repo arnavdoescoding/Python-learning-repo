191ec911349e844c0a3db3015ecaa77ffd1f7ebf
Arnav Bhagwat <arnavdoescoding@gmail.com>
2022-04-23 11:21:18 +0530

    Masking Incomplete

--- opencv/samplecv.py
+++ opencv/samplecv.py
@@ -1,11 +1,24 @@
+from cv2 import imshow
+import numpy as np
 import cv2 as cv
-from cv2 import threshold 
-import numpy as np 
-img = cv.imread('C:/Users/DELL/Desktop/Sample.png')
-img_g = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
-black = np.zeros(img.shape[:2], dtype='uint8')
-cv.imshow("Static Scan Greyscale", img_g)
-threshold, thresh = cv.threshold(img_g, 150 , 255 , cv.THRESH_BINARY)
-cv.imshow("Static Scan Greyscale", thresh)
+
+image = cv.imread('C:/Users/DELL/Desktop/Sample.png')
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+ret, thresh = cv.threshold(gray, 240, 255, cv.THRESH_BINARY)
+
+image[thresh == 255] = 0
+
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+erosion = cv.erode(image, kernel, iterations = 1)
+
+cv.imshow('E', erosion)
+new_image = cv.cvtColor(erosion , cv.COLOR_BGR2HSV) #We convert our image to hsv because BGR images are influenced by light falling , hue , saturation
+lower_red = np.array([0, 138 , 252])
+high_red = np.array([0 , 138, 255])
+mask = cv.inRange(erosion, lower_red, high_red)
+cv.imshow('Mask', mask)
+
 
 cv.waitKey(0)
+cv.destroyAllWindows()
