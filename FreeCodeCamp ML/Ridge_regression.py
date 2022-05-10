4a18dedb26fa83e4625b4ab9d63506d804281088
Arnav Bhagwat <arnavdoescoding@gmail.com>
2022-05-10 18:07:53 +0530

    Ridge Regression

--- FreeCodeCamp ML/Ridge_regression.py
+++ FreeCodeCamp ML/Ridge_regression.py
@@ -1,8 +1,8 @@
-from statistics import LinearRegression, linear_regression
 import mglearn
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 X, y = mglearn.datasets.load_extended_boston()
-X_train , y_train, X_test, y_test = train_test_split(X , y , random_state=0)
-lin = LinearRegression(X_train, y_train)
+X_train, y_train, X_test, y_test = train_test_split(X, y, random_state=0)
+ridge = Ridge().fit(X_train, y_train)
 
-print(lin.score(X_train, y_train))
+print(ridge.score(X_train, y_train))
